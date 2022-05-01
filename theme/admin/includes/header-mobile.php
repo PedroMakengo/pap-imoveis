6f735ae3fb4333cec339708ba77de15b8aae6368
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-05-01 16:38:32 -0300

    Terminado a primeira tela do admin

---
+++ theme/admin/includes/header-mobile.php
@@ -0,0 +1,46 @@
+<header class="header-mobile d-block d-lg-none">
+  <div class="header-mobile__bar">
+    <div class="container-fluid">
+      <div class="header-mobile-inner">
+        <a class="logo" href="index.html">
+          <img src="../assets/images/icon/logo.png" alt="CoolAdmin" />
+        </a>
+        <button class="hamburger hamburger--slider" type="button">
+          <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+          </span>
+        </button>
+      </div>
+    </div>
+  </div>
+  <nav class="navbar-mobile">
+    <div class="container-fluid">
+      <ul class="navbar-mobile__list list-unstyled">
+        <li class="active has-sub">
+          <a class="js-arrow" href="#">
+            <i class="fas fa-home"></i>Página Inicial</a
+          >
+        </li>
+        <li>
+          <a href="#"> <i class="fas fa-users"></i>Usuários</a>
+        </li>
+        <li>
+          <a href="#"> <i class="fas fa-home"></i>Imóveis</a>
+        </li>
+        <li>
+          <a href="#"> <i class="far fa-bar-chart"></i>Estátisticas</a>
+        </li>
+        <li class="has-sub">
+          <a class="js-arrow" href="#">
+            <i class="fas fa-comments"></i>Feedback</a
+          >
+        </li>
+        <li class="has-sub">
+          <a class="js-arrow" href="?logout=true">
+            <i class="fas fa-power-off"></i>Sair</a
+          >
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>
---
+++ theme/admin/includes/side-bar.php
@@ -0,0 +1,37 @@
+<aside class="menu-sidebar d-none d-lg-block">
+        <div class="logo bg-white" style="border: 0; box-shadow: none">
+          <a href="#">
+            <img src="../assets/images/icon/logo.png" alt="Cool Admin" />
+          </a>
+        </div>
+        <div class="menu-sidebar__content js-scrollbar1">
+          <nav class="navbar-sidebar">
+            <ul class="list-unstyled navbar__list">
+              <li class="active has-sub">
+                <a class="js-arrow" href="#">
+                  <i class="fas fa-home"></i>Página Inicial</a
+                >
+              </li>
+              <li>
+                <a href="#"> <i class="fas fa-users"></i>Usuários</a>
+              </li>
+              <li>
+                <a href="#"> <i class="fas fa-home"></i>Imóveis</a>
+              </li>
+              <li>
+                <a href="#"> <i class="far fa-bar-chart"></i>Estátisticas</a>
+              </li>
+              <li class="has-sub">
+                <a class="js-arrow" href="#">
+                  <i class="fas fa-comments"></i>Feedback</a
+                >
+              </li>
+              <li class="has-sub">
+                <a class="js-arrow" href="?logout=true">
+                  <i class="fas fa-power-off"></i>Sair</a
+                >
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </aside>
