4b859c652ee2c90151a3c9637f040d498667d6c1
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-05-01 19:10:54 -0300

    Trabalhando na parte do comprador

---
+++ theme/arrendador/feedback.php
@@ -0,0 +1,49 @@
+<!-- Head -->
+<?php require 'includes/head.php' ?>
+<!-- End Head -->
+
+  <body class="animsition">
+    <div class="page-wrapper">
+      <!-- HEADER MOBILE-->
+      <?php require 'includes/header-mobile.php' ?>
+      <!-- END HEADER MOBILE-->
+
+      <!-- MENU SIDEBAR-->
+      <?php require 'includes/side-bar.php' ?>
+      <!-- END MENU SIDEBAR-->
+
+      <!-- PAGE CONTAINER-->
+      <div class="page-container">
+        <!-- HEADER DESKTOP-->
+        <?php require 'includes/header.php' ?>
+        <!-- HEADER DESKTOP-->
+
+        <!-- MAIN CONTENT-->
+        <div class="main-content">
+          <div class="section__content section__content--p30">
+            <div class="container-fluid">
+              <!-- Estatistica -->
+              <div class="row m-t-25">
+                <div class="col-lg-12">
+                  <div class="bg-white p-4">
+                    <h1 class="h6">Formulário</h1>
+                  </div>
+                </div>
+              </div>
+              <!-- Estatistica -->
+            </div>
+          </div>
+        </div>
+        <!-- END MAIN CONTENT-->
+
+        <!-- END PAGE CONTAINER-->
+      </div>
+    </div>
+   
+    <!-- Footer -->
+    <?php require 'includes/footer.php' ?>
+    <!-- End Footer -->
+
+  </body>
+</html>
+<!-- end document-->
