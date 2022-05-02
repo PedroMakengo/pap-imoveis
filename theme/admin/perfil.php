0e4f0378d9d1a83a3ff16b9a87aa3ac90af7b6a2
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-05-02 15:43:40 -0300

    Trabalhando

--- theme/admin/perfil.php
+++ theme/admin/perfil.php
@@ -27,38 +27,104 @@
                 <div class="col-lg-4">
                  <!-- Perfil -->
                  <div class="card">
+                   <?php
+                    $parametros = [":id" => $_SESSION['id']];
+                    $buscandoPerfil = new Model();
+                    $buscando = $buscandoPerfil->EXE_QUERY("SELECT * FROM tb_admin WHERE id_admin=:id", $parametros);
+                    foreach($buscando as $mostrar):
+                      $nome     = $mostrar['nome'];
+                      $email    = $mostrar['email'];
+                      $foto     = $mostrar['foto'];
+                    endforeach;
+                   ?>
                     <div class="card-body">
                         <div class="mx-auto d-block">
-                            <img class="rounded-circle mx-auto d-block" src="../assets/images/icon/avatar-01.jpg" alt="Card image cap">
-                            <h5 class="text-sm-center mt-2 mb-1">Steven Lee</h5>
+                            <img class="rounded-circle mx-auto d-block" style="width: 90px; height: 90px;" src="../assets/images/icon/<?= $foto ?>" alt="Card image cap">
+                            <h5 class="text-sm-center mt-2 mb-1"><?= $nome ?></h5>
                             <div class="location text-sm-center">
-                                <i class="fa fa-map-marker"></i> California, United States</div>
-                        </div>
-                        <hr>
-                        <div class="card-text text-sm-center">
-                            <a href="#">
-                                <i class="fa fa-facebook pr-1"></i>
-                            </a>
-                            <a href="#">
-                                <i class="fa fa-twitter pr-1"></i>
-                            </a>
-                            <a href="#">
-                                <i class="fa fa-linkedin pr-1"></i>
-                            </a>
-                            <a href="#">
-                                <i class="fa fa-pinterest pr-1"></i>
-                            </a>
+                              <?= $email ?>  
+                            </div>
                         </div>
                     </div>
                     <div class="card-footer text-center">
-                        <strong class="card-title mb-3">Profile Card</strong>
+                        <strong class="card-title mb-3">Administrador</strong>
                     </div>
                   </div>
                   <!-- End Perfil -->
                 </div>
                 <div class="col-lg-8">
                   <div class="bg-white p-4">
-                    <h1 class="h6">Formulário</h1>
+                    <form method="POST">
+                      <div class="row">
+                        <?php 
+                          $parametros = [":id" => $_SESSION['id']];
+                          $buscandoPerfilAdmin = new Model();
+                          $buscando = $buscandoPerfilAdmin->EXE_QUERY("SELECT * FROM tb_admin WHERE id_admin=:id", $parametros);
+                          foreach($buscando as $mostrar):
+                        ?>
+                          <div class="col-lg-6 form-group">
+                            <label for="">Nome Completo:</label>
+                            <input type="text" value="<?= $mostrar['nome'] ?>" class="form-control" name="nome" />
+                          </div>
+                          <div class="col-lg-6 form-group">
+                            <label for="">E-mail:</label>
+                            <input type="email" value="<?= $mostrar['email'] ?>" class="form-control" name="email" />
+                          </div>
+                          <div class="col-lg-6 form-group">
+                            <label for="">Palavra-passe:</label>
+                            <input type="password" class="form-control" name="senha" />
+                          </div>
+                          <div class="col-lg-6 form-group">
+                            <label for="">Foto:</label>
+                            <input type="file" class="form-control" name="foto" />
+                          </div>
+                        <?php
+                        endforeach;
+                        ?>
+                        <div class="col-lg-4 form-group">
+                          <input type="submit" value="Atualizar" class="form-control bg-primary text-white btn" name="atualizarPerfil" />
+                        </div>
+
+                        <!-- Atualizar Perfil -->
+                        <?php 
+                          if(isset($_POST['atualizarPerfil'])):
+                            $nome = $_POST['nome'];
+                            $email = $_POST['email'];
+                            $senha = $_POST['senha'] === '' ? $mostrar['senha'] : md5(md5($_POST['senha']));
+
+                            $target        = "../assets/images/icon/" . basename($_FILES['foto']['name']);
+                            $foto          = $_FILES['foto']['name'] === '' ? $mostrar['foto'] : $_FILES['foto']['name'];
+
+                            $parametros = [
+                              ":id"       => $_SESSION['id'],
+                              ":nome"     => $nome, 
+                              ":email"    => $email,
+                              ":senha"    => $senha,
+                              ":foto"     => $foto   
+                            ];
+        
+                            $atualizarMeuPerfil = new Model();
+                            $atualizarMeuPerfil->EXE_NON_QUERY("UPDATE tb_admin SET
+                              nome=:nome,
+                              email=:email,
+                              senha=:senha,
+                              foto=:foto
+                              WHERE id_admin=:id
+                            ", $parametros);
+        
+                            if($atualizarMeuPerfil):
+                              if (move_uploaded_file($_FILES['foto']['tmp_name'], $target)) :
+                                $sms = "Uploaded feito com sucesso";
+                              else:
+                                  $sms = "Não foi possível fazer o upload";
+                              endif;
+                              echo "<script>location.href='perfil.php?id=perfil'</script>";
+                            endif;
+                          endif;
+                        ?>
+                        <!-- Endi Perfil -->
+                      </div>
+                    </form>
                   </div>
                 </div>
               </div>
